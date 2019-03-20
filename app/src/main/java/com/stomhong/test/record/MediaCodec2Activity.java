ad7cc1bf1bad1766c1e607302ee1aa67eabd098a
stomhong <stomhong@163.com>
2019-03-20 18:00:47 +0800

    commit code

---
+++ app/src/main/java/com/stomhong/test/record/MediaCodec2Activity.java
@@ -0,0 +1,124 @@
+package com.stomhong.test.record;
+
+import android.graphics.ImageFormat;
+import android.hardware.Camera;
+import android.media.MediaCodec;
+import android.media.MediaCodecInfo;
+import android.media.MediaFormat;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import com.stomhong.test.R;
+
+import java.io.IOException;
+
+public class MediaCodec2Activity extends AppCompatActivity implements SurfaceHolder.Callback,
+        Camera.PreviewCallback {
+
+    private static final String TAG = MediaCodec2Activity.class.getSimpleName();
+
+    private Camera.Parameters mParameters;
+    private Camera mCamera;
+    private SurfaceHolder mHolder;
+    private SurfaceView mSurfaceView;
+
+    MediaCodec.BufferInfo mBufferInfo;
+    MediaCodec mEncoder;
+
+    // parameters for the encoder
+    private static final String MIME_TYPE = "video/avc";    // H.264 Advanced Video Coding
+    private static final int FRAME_RATE = 15;               // 15fps
+    private static final int IFRAME_INTERVAL = 10;          // 10 seconds between I-frames
+    private static final int NUM_FRAMES = 30;
+
+    private int mBitRate = -1;
+
+    private static final int VIDEO_WIDTH = 1920;
+    private static final int VIDEO_HEIGHT = 1080;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_media_codec);
+        init();
+    }
+
+    private void init() {
+        mSurfaceView = findViewById(R.id.surfaceView);
+        mSurfaceView.getHolder().addCallback(this);
+
+        mBufferInfo = new MediaCodec.BufferInfo();
+
+        //配置媒体格式
+        MediaFormat format = MediaFormat.createVideoFormat(MediaFormat.MIMETYPE_VIDEO_AVC, VIDEO_WIDTH, VIDEO_HEIGHT);
+        format.setInteger(MediaFormat.KEY_COLOR_FORMAT, MediaCodecInfo.CodecCapabilities.COLOR_FormatSurface);//颜色
+        format.setInteger(MediaFormat.KEY_BIT_RATE, mBitRate);//比特率
+        format.setInteger(MediaFormat.KEY_FRAME_RATE, FRAME_RATE);//帧率
+        format.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, IFRAME_INTERVAL);//I帧间隔，I帧间隔越小视频压缩率越低,视频就会越大
+        try {
+            //创建编码器
+            mEncoder = MediaCodec.createEncoderByType(MIME_TYPE);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //配置编码器
+        mEncoder.configure(format,null,null,0);
+        //启动编码器
+        mEncoder.start();
+    }
+
+    /**
+     * 打开摄像头
+     * @param position 摄像头位置
+     */
+    public void openCamera(int position, SurfaceHolder holder){
+        mCamera = Camera.open(position);//打开摄像头
+        mParameters = mCamera.getParameters();//设置参数
+        mParameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);//设置自动对焦
+        try {
+            mParameters.setPreviewFormat(ImageFormat.NV21);
+            mParameters.setPreviewSize(1920,1080);//设置预览尺寸，为了全屏展示，我们选择最大尺寸，同时TextureView也应该是match_parent全屏
+            mCamera.setParameters(mParameters);//设置相机的参数
+            mCamera.setDisplayOrientation(90);//设置显示翻转，为0则是水平录像，90为竖屏
+            mCamera.setPreviewDisplay(mSurfaceView.getHolder());//将onSurfaceTextureAvailable监听中的surface传入进来，设置预览的控件
+        } catch (IOException t) {
+            Log.d(TAG, "onSurfaceTextureAvailable: IO异常");
+        }
+        mCamera.startPreview();//开始预览
+        mCamera.setPreviewCallback(this);
+    }
+
+    /**
+     * 释放摄像头资源
+     */
+    public void releaseCamera(){
+        mCamera.setPreviewCallback(null);
+        mCamera.stopPreview();
+        mCamera.release();
+        mCamera = null;
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        openCamera(Camera.CameraInfo.CAMERA_FACING_FRONT,holder);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        releaseCamera();
+    }
+
+    @Override
+    public void onPreviewFrame(byte[] data, Camera camera) {
+
+    }
+}
